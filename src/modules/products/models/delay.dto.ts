e34b90e2a098b69d880b0cb9b39462191ddb2929
Vlad Belikov <v.belikov@aisnovations.com>
2023-02-18 15:31:56 +0300

    added transform

--- src/modules/products/models/delay.dto.ts
+++ src/modules/products/models/delay.dto.ts
@@ -1,9 +1,11 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsOptional, IsPositive } from 'class-validator';
+import { Transform, TransformFnParams } from 'class-transformer';
 
 export class Delay {
   @IsOptional()
   @IsPositive()
+  @Transform(({ value }: TransformFnParams) => +value)
   @ApiPropertyOptional({ description: 'Response delay' })
   delay?: number;
 }
